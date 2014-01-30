e6ab6ac1b718aed48740d934da3cc1a90b8c78af
Andrew <neoandrew@yandex.ru>
2014-01-30 19:10:39 +0200

    v0.02

--- word.js
+++ word.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 var vowel = String ('аеёиоуыэюяАЕЁИОУЫЭЮЯ'); //Вводим гласные буквы
-function getVowelString (s) {
+var cons  = String ('бвгджзйклмнпрстфхцчшщБВГДЖЗКЛМНПРСТФХЦЧШЩ'); //Вводим согласные буквы
+function getVowelString (s) { //Функция отсеивания гласных букв
 	var tmpl = String (); //Текущий символ
 	var vowelArr = new Array (); //Массив гласных букв
 	for (var i = 0; i < s.length; i++) {
@@ -9,4 +10,16 @@ function getVowelString (s) {
 			vowelArr.push (tmpl); continue;
 		} //if (vowel.indexOf (tmpl) !=-1)
 	} //for (var i = 0; i < s.length; i++)
+	return vowelArr.join('|');
 } //function getVowelString (s)
+function getConsString (s) { //Функция отсеивания согласных букв
+	var tmpl = String (); //Текущий символ
+	var consArr = new Array (); //Массив согласных букв
+	for (var i = 0; i < s.length; i++) {
+		tmpl = s.substr (i,1);
+		if (cons.indexOf (tmpl) !=-1) {
+			consArr.push (tmpl); continue;
+		} //if (cons.indexOf (tmpl) !=-1)
+	} //for (var i = 0; i < s.length; i++)
+	return consArr.join('|');
+} //function getConsString (s)
